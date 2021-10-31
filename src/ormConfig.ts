7b20709157db82f4bcd3751a441377f9f3b7f239
Ramprit Sahani <palak.ghai@torinit.ca>
2021-10-31 20:04:41 +0530

    Adding config

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,17 +1,17 @@
-import { ConnectionOptions } from "typeorm";
+import { ConnectionOptions } from 'typeorm';
 
 const ormConfig: ConnectionOptions = {
-  type: "postgres",
-  host: "localhost",
+  type: 'postgres',
+  host: process.env.HOST || 'localhost',
   port: 5432,
-  username: "postgres",
-  password: "plumtree",
-  database: "mediumclone",
-  entities: ["**/*.entity{.ts,.js}"],
+  username: process.env.USER_NAME || 'postgres',
+  password: process.env.PASSWORD || 'plumtree',
+  database: process.env.DATABASE || 'mediumclone',
+  entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: false,
-  migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
   cli: {
-    migrationsDir: "src/migrations"
-  }
+    migrationsDir: 'src/migrations',
+  },
 };
 export default ormConfig;
